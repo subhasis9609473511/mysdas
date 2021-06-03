1cfd292f5fb441b226f3d5beb2c6f249833f0f00
unknown <subhasis.bluehorse@gmail.com>
2021-06-03 23:02:27 +0530

    test test

---
+++ app/Http/Middleware/CheckAge.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+
+class CheckAge
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \Closure  $next
+     * @return mixed
+     */
+    public function handle(Request $request, Closure $next)
+    {
+    	if ($request->age <= 18) 
+    	{
+            echo "Under Age";
+            abort(403);
+        }
+        if ($request->age > 20) 
+    	{
+            return $next($request);
+        }
+
+   
+    }
+}
